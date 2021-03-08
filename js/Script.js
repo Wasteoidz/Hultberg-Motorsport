7536aa2a01fb4a848efee9d50bd6f6bd52f23e3f
Wasteoidz <71650988+Wasteoidz@users.noreply.github.com>
2021-03-08 02:12:52 +0100

    Menu update

--- js/Script.js
+++ js/Script.js
@@ -1,1 +1,4 @@
-// JavaScript source code
+function linn() {
+    document.querySelector(".middle1").innerHTML = `<p>linn er den andre Sjaføren for Team hultberg, <br>
+                                                     hun følger i Bjørns fotspor og vi kan forvente mye bra i de kommende sesongene fra henne.</p>`
+}// JavaScript source code
