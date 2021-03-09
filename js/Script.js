2d326ba01eae5fafd86b306d1693c00cd2e72ad5
Wasteoidz <71650988+Wasteoidz@users.noreply.github.com>
2021-03-09 15:24:33 +0100

    menu progression

--- js/Script.js
+++ js/Script.js
@@ -1,4 +1,13 @@
+function Bjørn() {
+    document.querySelector(".middle1").innerHTML = `<p>Bjørn Hultnerg er grunnleggeren av teamet og en legende innen norsk og europeisk drag race</p>
+    
+    `;
+}
+
+
+
 function linn() {
-    document.querySelector(".middle1").innerHTML = `<p>linn er den andre Sjaføren for Team hultberg, <br>
-                                                     hun følger i Bjørns fotspor og vi kan forvente mye bra i de kommende sesongene fra henne.</p>`
+    document.querySelector(".middle1").innerHTML = `<p>Linn er den andre Sjaføren for Team hultberg, <br>
+                                                     hun følger i Bjørns fotspor og vi kan forvente mye bra i de kommende sesongene.</p>
+    `;
 }// JavaScript source code
