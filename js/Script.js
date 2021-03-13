51539328f309f1f4e229f4f43ea0e9519cac7782
Wasteoidz <71650988+Wasteoidz@users.noreply.github.com>
2021-03-13 23:02:20 +0100

    Member info update

--- js/Script.js
+++ js/Script.js
@@ -1,5 +1,5 @@
 function Bjørn() {
-    document.querySelector(".middle1").innerHTML = `<p>Bjørn Hultnerg er grunnleggeren av teamet og en legende innen norsk og europeisk drag race</p>
+    document.querySelector(".middle1").innerHTML = `<p>Bjørn Hultberg er grunnleggeren av teamet og en legende innen norsk og europeisk drag race</p>
     
     `;
 }
@@ -13,5 +13,21 @@ function linn() {
 function gitte() {
     document.querySelector(".middle1").innerHTML = `<p>Gitte er Crew sjef og holder teamet i ørene, hun sørger for at ting blir gjort som de skal og til riktig tid.<br>
                                                       Samtidig som hun også passer på at alle har det bra, at ingen går sultne eller kalde.</p>
+    `;
+}
+
+function Kåre() {
+    document.querySelector(".middle1").innerHTML = `<p>Kåre Har vært med siden tidenes morgen. Teamets smartepetter og mattematiker.<br>
+                                                       Ikke bare det han er også en grise god kokk!</p>
+    `;
+}
+
+function Christoffer() {
+    document.querySelector(".middle1").innerHTML = `<p>Christoffer er en liten virrevapp som er utrolig dedikert. Står på. Oppservang. Lett på foten</p>
+    `;
+}
+
+function Nina() {
+    document.querySelector(".middle1").innerHTML = `<p>Nina har alltid ett smil om munnen, den gode hjelperen som ikke gjør så mye ut av seg men som vi absolutt ikke hadde klart oss uten.</p>
     `;
 }
